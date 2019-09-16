ce4eb52c43a768d0b9329166d587c51c682695ad
Anya <anya.vatamanyuk@gmail.com>
2019-09-16 09:01:40 +0300

    add changes

--- markup/src/js/pages/home.js
+++ markup/src/js/pages/home.js
@@ -17,6 +17,38 @@ $(function () {
   });
 });
 
+$(function () {
+  $('.quote-slide .slider .slider-content').slick({
+    dots:true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    rows: 1,
+    responsive:[
+      {
+        breakpoint:1151,
+        settings:{
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint:921,
+        settings:{
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint:580,
+        settings:{
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
+
 $(function(){
   navOpener();
   rangeSlider();
